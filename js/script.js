12a96365f24393112f6d77b47ba760e5d27bf1dd
kaquoc <kaquoc1206@gmail.com>
2022-07-03 17:32:52 -0400

    Working eye animation.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-var eyeBall = document.querySelector(".pupil");
+var eyeBall = document.querySelector('#pupil_left');
+var eyeBall2 = document.querySelector('#pupil_right');
+//document query selector return the first Element within the document by using DFS pre-order traversal of DOM tree
+//DOM tree: Document Object Node, tree representation of the web page that gets loaded into the browser. 
+//Represents the web page as a series of object, with each object housing their own object
+
 
 
 document.onmousemove = (event) => {
@@ -6,13 +11,27 @@ document.onmousemove = (event) => {
     var y = event.clientY * 100 / window.innerHeight + "%";
     
 
-    console.log(eyeBall == null);
-    console.log(eyeBall);
+    // LEFT EYEBALL
+    eyeBall.style.left = x;
+    eyeBall.style.top = y;
+    eyeBall.style.transition = "0s";
+
+
+    // RIGHT EYEBALL
+    eyeBall2.style.left = x;
+    eyeBall2.style.top = y;
+    eyeBall2.style.transition = "0s";
+
+    
 }
 
 document.onmouseout = (event) => {
-    eyeBall.transition = "0.7s";
-    eyeBall.left = "50%";
-    eyeBall.top = "50%";
+    eyeBall.style.left = "50%";
+    eyeBall.style.top = "50%";
+    eyeBall.style.transition = "0.7s";
+
+    eyeBall2.style.left = "50%";
+    eyeBall2.style.top = "50%";
+    eyeBall2.style.transition = "0.7s";
 }
 
