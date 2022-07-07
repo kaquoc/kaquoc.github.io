11ff562118c521fb4a7d7c39ec5fa40efe8d90c6
kaquoc <kaquoc1206@gmail.com>
2022-07-07 16:06:14 -0400

    add API request using JQuerry

--- js/FPpage.js
+++ js/FPpage.js
@@ -8,10 +8,54 @@ button.addEventListener("click",() => {
  * 
  */
 window.navigator.geolocation
-  .getCurrentPosition(success,console.log);
+  .getCurrentPosition(success,showError);
 });
 
+//callback function to handle success
+/**getCurrentPosition return an coords object when success
+ * which could be access using coords.longitude or coords.latitude
+ */
 function success(position) {
-    console.log(position);
-}
+    var longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+    console.log(longitude);
 
+    
+    //sending a request using JQuerry
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://adsbx-flight-sim-traffic.p.rapidapi.com/api/aircraft/json/"
+        +longitude+ "/%7Blat%7D/"+
+        latitude+
+        "/%7Blon%7D/dist/25/",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "9a977024b3msh661b55182095c6cp18eed5jsn8f12205dd548",
+            "X-RapidAPI-Host": "adsbx-flight-sim-traffic.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+
+
+}
+//callback function to handle error
+function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+       console.log("An unknown error occurred.");
+        break;
+    }
+  }
